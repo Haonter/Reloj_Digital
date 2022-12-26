24246a5903de4d45d8d5f6fbe0eb86d3e3cc28eb
Haonter <101908524+Haonter@users.noreply.github.com>
2022-12-26 17:49:02 -0300

    Cambio  de formato Horario (24h a 12h)

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 function horaActual(){
+    /*Obtener Fecha y Hora completa*/
     let fecha = new Date();
+    /*Seccionar Informacion de Hora*/
     let hora = fecha.getHours();
     let minutos = fecha.getMinutes();
     let segundos = fecha.getSeconds();
     /*let milisegundos = fecha.getMilliseconds();*/
 
+    /*Obtener Elementos HTML*/
     let horaReloj = document.getElementById("Hora");
     let minutoReloj = document.getElementById("Minuto");
     let segundoReloj = document.getElementById("Segundo");
     /*milesimaReloj = document.getElementById("Milisegundo");*/
-    
     let fechaReloj = document.getElementById("Fecha");
 
-
+    /*Mostrar Hora: Formato 12h */
     if(hora < 10){
         horaReloj.innerHTML = "0" + hora;
     }
-    else{
-        horaReloj.innerHTML = hora;
+    else if (hora > 12){
+        horaReloj.innerHTML = "0" + (hora - 12);
     }
 
+    /*Mostrar Minutos*/
     if(minutos < 10){
         minutoReloj.innerHTML = "0" + minutos;
     }
@@ -27,23 +30,23 @@ function horaActual(){
         minutoReloj.innerHTML = minutos;
     }
 
-    if(segundos < 10){
-        if(hora <= 12){
-            segundoReloj.innerHTML = "0" + segundos + " AM";
-        }else if(hora>12){
-            segundoReloj.innerHTML = "0" + segundos + " PM";
-        }
+    /*Mostras Segundos y asignar Tiempo horario*/
+    if(segundos < 10 && hora <= 12){
+        segundoReloj.innerHTML = "0" + segundos + " AM";
     }
-    else{
-        if(hora <= 12){
-            segundoReloj.innerHTML = segundos + " AM";
-        }else if(hora>12){
-            segundoReloj.innerHTML = segundos + " PM";
-        }
+    else if(segundos < 10 && hora > 12){
+        segundoReloj.innerHTML = "0" + segundos + " PM";
+    }
+    else if(hora <= 12){
+        segundoReloj.innerHTML = segundos + " AM";
+    }
+    else if(hora>12){
+        segundoReloj.innerHTML = segundos + " PM";
     }
 
     /*milesimaReloj.innerHTML = milisegundos;*/
 
+    /*Mostrar Fecha*/
     fechaReloj.innerHTML = fecha.toLocaleDateString();
 }
 
